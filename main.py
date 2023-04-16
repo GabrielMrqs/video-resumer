a21167b1d65a15a741124789f12e7676011466be
Gabriel Marques <gabrielmrqs.contato@gmail.com>
2023-04-15 21:50:32 -0300

    refactor

--- main.py
+++ main.py
@@ -10,15 +10,11 @@ openai.api_key = API_KEY
 
 link = 'https://www.youtube.com/watch?v=H1HdZFgR-aA'
 
-video_id = link.split('watch?v=')[1]
-
-file_name = f'{video_id}.mp4'
-
 yt = YouTube(link)
 
 stream = yt.streams.get_audio_only()
 
-file_path = stream.download(filename=file_name)
+file_path = stream.download(filename='video.mp4')
 
 model = whisper.load_model(WHISPER_MODEL_NAME)
 
@@ -26,7 +22,7 @@ result = model.transcribe(file_path)
 
 text = result['text']
 
-os.remove(file_name)
+os.remove(file_path)
 
 response_chat_gpt = openai.ChatCompletion.create(
     model="gpt-3.5-turbo",
@@ -39,3 +35,5 @@ response_chat_gpt = openai.ChatCompletion.create(
 response = ''
 for choice in response_chat_gpt.choices:
     response += choice.message.content
+
+print(response)
